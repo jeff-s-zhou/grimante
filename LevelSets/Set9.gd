d6b4b73ecbcaaf5fb4c5572a4002bdd0a59cfcf8
Jeffrey Zhou <jeff.s.zhou@gmail.com>
2018-02-02 17:16:49 -0700

    final two levels playtested

--- LevelSets/Set9.gd
+++ LevelSets/Set9.gd
@@ -18,19 +18,19 @@ func final_level():
 	var pieces
 	var score_guide
 	pieces = load_level("final_level.level")
-	score_guide = {1:5, 2:5, 3:5, 4:4, 5:3, 6:2} #not tested
+	score_guide = {1:5, 2:5, 3:5, 4:4, 5:3, 6:2} 
 	var raw_enemies = pieces[0]
 	var allies = pieces[1]
 	var enemies = EnemyWrappers.FiniteCuratedWrapper.new(raw_enemies)
 	var extras = {"free_deploy":false, "flags":[], "score_guide":score_guide}
-	return LevelTypes.Timed.new(52, "Penultimatum", allies, enemies, 6, null, extras)
+	return LevelTypes.Timed.new(52, "Penultimayhem", allies, enemies, 6, null, extras)
 	
 	
 func final_level2():
 	var pieces
 	var score_guide
 	pieces = load_level("final_level2.level")
-	score_guide = {1:5, 2:5, 3:5, 4:4, 5:3, 6:2} #not tested
+	score_guide = {1:5, 2:5, 3:4, 4:3, 5:2}
 	var raw_enemies = pieces[0]
 	var allies = pieces[1]
 	var enemies = EnemyWrappers.FiniteCuratedWrapper.new(raw_enemies)
